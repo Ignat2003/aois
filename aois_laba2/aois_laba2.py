031edd6f48af01b6c86a32a4d839951cd3787f8c
Ignat2003 <ignat.gotin@gmail.com>
2023-03-08 18:15:37 +0300

    структурировал

--- aois_laba2/aois_laba2.py
+++ aois_laba2/aois_laba2.py
@@ -1,52 +1,7 @@
 import re
 
 
-infunc = input('Enter your function: ')
-# infunc = '((-a)*(s=b))'
-infunc = infunc.replace("=", "==")
-
-variables = sorted(set(re.findall(r"[A-Za-z]", infunc)))
-
-# просто красивое оформление для таблицы
-header = [""] * 2
-for key in variables:
-    header[0] += "-" * 7 + "+"
-    header[1] += f"   {key}   |"
-header[0] += "-+" + "-" * 7
-header[1] += " | Result"
-print("\n".join(header + header[0:1]))
-
-vars_for_eval = {}
-disjunctive_normal_form_safe = []
-conjunctive_normal_form = []
-
-# вариантов входных значений для таблицы - 2 в степени кол-ва переменных
-for variant in range(1 << len(variables)):
-    # заполняем входной словарь c представлением переменных
-    # key идут в прямом порядке, а i - в обратном
-    for i, key in reversed(list(enumerate(reversed(variables)))):
-        # используем биты этого числа для инициализыции булевых значений
-        vars_for_eval[key] = bool(variant & (1 << i))
-        # вывод строки таблицы истинности
-        print(f" {vars_for_eval[key]:<5}", end=" |")
-    # вычисляем результат
-    # infunc = infunc.replace('-', ' not ')
-    # infunc = infunc.replace('+', ' or ')
-
-    func = infunc[:]
-    # Замена символов на True and False
-    for variable, is_true in vars_for_eval.items():
-        func = func.replace(variable, f' {variable} ')
-
-    for variable, is_true in vars_for_eval.items():
-        is_true = str(bool(is_true))
-        func = func.replace(f' {variable} ', is_true)
-
-
-    if func.count('(') != func.count(')'):
-        raise Exception("Wrong amount of staples")
-
-    # Вычисление ответа
+def parse_func(func):
     while '(' in func:
         first_parenthesis = None
         last_parenthesis = func.find(')')
@@ -59,8 +14,7 @@ for variant in range(1 << len(variables)):
         if first_parenthesis is None:
             raise print('Parse error didnt find first_parenthesis')
 
-        subexpression = func[first_parenthesis: last_parenthesis+1]
-
+        subexpression = func[first_parenthesis: last_parenthesis + 1]
 
         if '-' in subexpression:
             answer = 'True' if 'False' in subexpression else 'False'
@@ -90,46 +44,140 @@ for variant in range(1 << len(variables)):
         else:
             raise print('Parse error')
 
-
     if func == 'True':
-        disjunctive_normal_form_safe.append(vars_for_eval.copy())
+        # disjunctive_normal_form_safe.append(vars_for_eval.copy())
         result = 1
     else:
         result = 0
-        conjunctive_normal_form.append(vars_for_eval.copy())
+        # conjunctive_normal_form.append(vars_for_eval.copy())
+
+    return result
 
 
-    print(f" | {result:<5}")
-print(header[0])
+def vars_for_truth_table(variables):
+    vars_for_eval = {}
+    for variant in range(1 << len(variables)):
+        vars_for_eval[variant] = {}
+        # заполняем входной словарь c представлением переменных
+        # key идут в прямом порядке, а i - в обратном
+        for i, key in reversed(list(enumerate(reversed(variables)))):
+            # используем биты этого числа для инициализыции булевых значений
+            vars_for_eval[variant].update({key: str(bool(variant & (1 << i)))})
+            # vars_for_eval[key] = bool(variant & (1 << i))
 
-print('Disjunctive_normal_form')
-answer = ''
+    return vars_for_eval
 
-for form in disjunctive_normal_form_safe:
-    for variable, is_true in form.items():
-        print(' ' if is_true else '-',  end='')
-        answer += variable
-    print('   ', end='')
-    answer += ' ∨ '
-print()
-print(''.join(answer[:-2]))
 
-print()
-print('Conjunctive_normal_form')
-answer = ''
-for form in conjunctive_normal_form:
-    answer += '('
-    print(' ', end='')
+def change_symbols(func: str, vars_for_eval: dict):
+    # Замена символов на True and False
+    for variable, is_true in vars_for_eval.items():
+        func = func.replace(variable, f' {variable} ')
 
-    for variable, is_true in form.items():
-        print('- ' if is_true else '  ',  end='')
-        answer += variable
+    for variable, is_true in vars_for_eval.items():
+        func = func.replace(f' {variable} ', is_true)
 
-    print(' ', end='')
-    answer += ')∧'
-print()
-answer = ''.join(answer[:-1])
+    if func.count('(') != func.count(')'):
+        raise Exception("Wrong amount of staples")
+    return func
 
-for variable in variables[:-1]:
-    answer = answer.replace(variable, variable + '+')
-print(answer)
+
+def calculate_truth_table(func: str, vars_for_truth_table: dict, variables: list):
+    truth_table_dst = {}
+    for variant in vars_for_truth_table:
+        tmp_func = change_symbols(func[:], vars_for_truth_table[variant])
+        answer = parse_func(tmp_func[:])
+        truth_table_dst[variant] = answer
+
+    return truth_table_dst
+
+
+def print_table(vars_for_table: dict, truth_table_answer: dict, variables: list):
+    for v in variables:
+        print(f' {v} ', end='')
+    print('  Result')
+    for variant in vars_for_table:
+        for variable, mean in vars_for_table[variant].items():
+            mean = 1 if mean == 'True' else 0
+            print(f' {mean} ', end='')
+        print(f'  {truth_table_answer[variant]}')
+
+
+def print_disjunctive_normal_form(vars_for_table: dict, truth_table_answer: dict):
+    disjunctive_normal_form = []
+    for variant, mean in truth_table_answer.items():
+        if mean == 1:
+            disjunctive_normal_form.append(vars_for_table[variant])
+
+    answer = ''
+    for form in disjunctive_normal_form:
+        for variable, is_true in form.items():
+            print(' ' if is_true == 'True' else '-', end='')
+            answer += variable
+        print('   ', end='')
+        answer += ' ∨ '
+    print()
+    print(''.join(answer[:-2]))
+
+
+def print_conjunctive_normal_form(vars_for_table: dict, truth_table_answer: dict):
+    conjunctive_normal_form = []
+    for variant, mean in truth_table_answer.items():
+        if mean == 0:
+            conjunctive_normal_form.append(vars_for_table[variant])
+
+    answer = ''
+    for form in conjunctive_normal_form:
+        answer += '('
+        print(' ', end='')
+
+        for variable, is_true in form.items():
+            print('- ' if is_true == 'True' else '  ', end='')
+            answer += variable
+
+        print(' ', end='')
+        answer += ')∧'
+    print()
+    answer = ''.join(answer[:-1])
+
+    for variable in variables[:-1]:
+        answer = answer.replace(variable, variable + '+')
+    print(answer)
+
+
+def print_disjunctive_number_form(truth_table_answer: dict):
+    answer = []
+    for variant, mean in truth_table_answer.items():
+        if mean == 1:
+            answer.append(str(variant))
+    print('v(', end='')
+    a = ','.join(answer)
+    print(a, end='')
+    print(')')
+
+
+def print_conjunctive_number_form(truth_table_answer: dict):
+    answer = []
+    for variant, mean in truth_table_answer.items():
+        if mean == 0:
+            answer.append(str(variant))
+    print('∧(', end='')
+    a = ','.join(answer)
+    print(a, end='')
+    print(')')
+
+
+if __name__ == "__main__":
+    infunc = input('Enter your function: ')
+    infunc = infunc.replace("=", "==")
+    variables = sorted(set(re.findall(r"[A-Za-z]", infunc)))
+    vars_for_table = vars_for_truth_table(variables)
+    truth_table_answer = calculate_truth_table(infunc[:], vars_for_table, variables)
+    print_table(vars_for_table, truth_table_answer, variables)
+    print('Disjunctive_normal_form')
+    print_disjunctive_normal_form(vars_for_table, truth_table_answer)
+    print('Conjunctive_normal_form')
+    print_conjunctive_normal_form(vars_for_table, truth_table_answer)
+    print('Disjunctive_number_form')
+    print_disjunctive_number_form(truth_table_answer)
+    print('Conjunctive_number_form')
+    print_conjunctive_number_form(truth_table_answer)
