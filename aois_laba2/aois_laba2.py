ac1351e2f9a6106d9eb5e3e3bd2932226f5911bd
Ignat2003 <ignat.gotin@gmail.com>
2023-03-08 14:23:08 +0300

    сделал полностью новое вычисление

--- aois_laba2/aois_laba2.py
+++ aois_laba2/aois_laba2.py
@@ -2,11 +2,10 @@ import re
 
 
 infunc = input('Enter your function: ')
+# infunc = '((-a)*(s=b))'
 infunc = infunc.replace("=", "==")
-infunc = infunc.replace('>', '<=')
 
 variables = sorted(set(re.findall(r"[A-Za-z]", infunc)))
-
 
 # просто красивое оформление для таблицы
 header = [""] * 2
@@ -31,20 +30,69 @@ for variant in range(1 << len(variables)):
         # вывод строки таблицы истинности
         print(f" {vars_for_eval[key]:<5}", end=" |")
     # вычисляем результат
-    infunc = infunc.replace('-', ' not ')
-    infunc = infunc.replace('+', ' or ')
+    # infunc = infunc.replace('-', ' not ')
+    # infunc = infunc.replace('+', ' or ')
 
     func = infunc[:]
+    # Замена символов на True and False
+    for variable, is_true in vars_for_eval.items():
+        func = func.replace(variable, f' {variable} ')
+
     for variable, is_true in vars_for_eval.items():
         is_true = str(bool(is_true))
-        func = func.replace(variable, is_true)
+        func = func.replace(f' {variable} ', is_true)
 
-    result = eval(func)
-    result = 1 if result > 0 else 0
-    if result:
+
+    if func.count('(') != func.count(')'):
+        raise Exception("Wrong amount of staples")
+
+    # Вычисление ответа
+    while '(' in func:
+        last_parenthesis = func.find(')')
+        index = 0
+        for i in func[:last_parenthesis]:
+            if i == '(':
+                first_parenthesis = int(index)
+            index += 1
+
+        subexpression = func[first_parenthesis: last_parenthesis+1]
+
+        if '-' in subexpression:
+            answer = 'True' if 'False' in subexpression else 'False'
+            func = func.replace(subexpression, answer, 1)
+        elif '==' in subexpression:
+            answer = 'True' if subexpression.count('False') == 2 or subexpression.count('True') == 2 else 'False'
+            func = func.replace(subexpression, answer, 1)
+
+        elif '+' in subexpression:
+            answer = 'True' if 'True' in subexpression else 'False'
+            func = func.replace(subexpression, answer, 1)
+
+        elif '*' in subexpression:
+            answer = 'True' if subexpression.count('True') == 2 else 'False'
+            func = func.replace(subexpression, answer, 1)
+
+        elif '>' in subexpression:
+            first_symbol, second_symbol = subexpression.split('>')
+            if 'True' in second_symbol:
+                answer = 'True'
+            elif 'False' in first_symbol:
+                answer = 'True'
+            else:
+                answer = 'False'
+            func = func.replace(subexpression, answer, 1)
+
+        else:
+            raise print('Parse error')
+
+
+    if func == 'True':
         disjunctive_normal_form_safe.append(vars_for_eval.copy())
+        result = 1
     else:
+        result = 0
         conjunctive_normal_form.append(vars_for_eval.copy())
+
 
     print(f" | {result:<5}")
 print(header[0])
