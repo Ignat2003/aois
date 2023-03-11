a5c04d7d8eaaab5cdefba88458382da70b869332
Ignat2003 <ignat.gotin@gmail.com>
2023-03-12 00:30:40 +0300

    правки

--- LR3/aois_laba3.py
+++ LR3/aois_laba3.py
@@ -1,5 +1,6 @@
 import re
 
+
 def find_index_parenthesis(func: str):
     first_parenthesis = None
     last_parenthesis = func.find(')')
@@ -85,106 +86,111 @@ def calculate_truth_table(func: str, vars_for_truth_table: dict, variables: list
     return truth_table_dst
 
 
-def print_table(vars_for_table: dict, truth_table_answer: dict, variables: list):
-    for v in variables:
-        print(f' {v} ', end='')
-    print('  Result')
-    for variant in vars_for_table:
-        for variable, mean in vars_for_table[variant].items():
-            mean = 1 if mean == 'True' else 0
-            print(f' {mean} ', end='')
-        print(f'  {truth_table_answer[variant]}')
-
-
-def print_disjunctive_normal_form(vars_for_table: dict, truth_table_answer: dict):
+def get_disjunctive_normal_form(vars_for_table: dict, truth_table_answer: dict):
     disjunctive_normal_form = []
     for variant, mean in truth_table_answer.items():
         if mean == 1:
             disjunctive_normal_form.append(vars_for_table[variant])
 
-    answer = ''
-    for form in disjunctive_normal_form:
-        for variable, is_true in form.items():
-            print(' ' if is_true == 'True' else '-', end='')
-            answer += variable
-        print('   ', end='')
-        answer += ' ∨ '
-    print()
-    print(''.join(answer[:-2]))
+    return disjunctive_normal_form
 
 
-def print_conjunctive_normal_form(vars_for_table: dict, truth_table_answer: dict):
+def get_conjunctive_normal_form(vars_for_table: dict, truth_table_answer: dict):
     conjunctive_normal_form = []
     for variant, mean in truth_table_answer.items():
         if mean == 0:
             conjunctive_normal_form.append(vars_for_table[variant])
 
-    answer = ''
-    for form in conjunctive_normal_form:
-        answer += '('
-        print(' ', end='')
-
-        for variable, is_true in form.items():
-            print('- ' if is_true == 'True' else '  ', end='')
-            answer += variable
-
-        print(' ', end='')
-        answer += ')∧'
-    print()
-    answer = ''.join(answer[:-1])
-
-    for variable in variables[:-1]:
-        answer = answer.replace(variable, variable + '+')
-    print(answer)
+    return conjunctive_normal_form
 
 
-def print_disjunctive_number_form(truth_table_answer: dict):
-    answer = []
-    for variant, mean in truth_table_answer.items():
-        if mean == 1:
-            answer.append(str(variant))
-    print('v(', end='')
-    a = ','.join(answer)
-    print(a, end='')
-    print(')')
+def sub_gluing(first_element:dict, second_element:dict):
+    ans = []
+    count_of_equal = 0
+    length = len(first_element)
+    for i, j in zip(first_element.items(), second_element.items()):
+        el1, mean1 = i
+        el2, mean2 = j
+        if el1 == el2 and mean1 == mean2:
+            count_of_equal += 1
+            ans.append((el1, mean1))
+
+    if len(ans) == length-1:
+        return dict(ans)
+    else:
+        return None
 
 
-def print_conjunctive_number_form(truth_table_answer: dict):
-    answer = []
-    for variant, mean in truth_table_answer.items():
-        if mean == 0:
-            answer.append(str(variant))
-    print('∧(', end='')
-    a = ','.join(answer)
-    print(a, end='')
-    print(')')
+def gluing(disjunctive_form):
+    ans = []
+    for i in range(len(disjunctive_form)-1):
+        first_element = disjunctive_form[i]
+        for j in range(i+1, len(disjunctive_form)):
+            second_element = disjunctive_form[j]
+            result = sub_gluing(first_element, second_element)
+            if result is not None and result not in ans:
+                ans.append(result)
+    ans = drop_duplicates_in_list(ans)
+    return ans
 
-def print_index_form(truth_table_answer: dict):
-    degree = len(truth_table_answer) - 1
-    weights = []
-    for variant, mean in truth_table_answer.items():
-        if mean == 1:
-            weights.append(2 ** degree)
-        degree -= 1
-    return sum(weights)
 
+def drop_duplicates_in_list(sam_list):
+    result = []
+    for i in sam_list:
+        if i not in result:
+            result.append(i)
+    return result
+
+def get_variable_that_not_in_impicant(implicant:dict, variables:list):
+    a = list(set(variables.copy()) - set(implicant.copy().keys()))
+    return ''.join(a)
+
+
+def drop_extra_impicants(disjunctive_form: list, variables: list):
+    func = ''
+    ans = []
+    for implicant in disjunctive_form:
+        for variable, mean in implicant.items():
+            func += variable if mean == 'True' else f'!{variable}'
+
+        func += ' + '
+    func = func[:-3]
+    for implicant in disjunctive_form:
+        variable_that_not_in_impicant = get_variable_that_not_in_impicant(implicant, variables)
+        tmp_func = ''
+        for variable, mean in implicant.items():
+            tmp_func += variable if mean == 'True' else f'!{variable}'
+
+        main_func = func.replace(tmp_func, '', 1)
+
+        for variable, mean in implicant.items():
+            mean = '1' if mean == 'True' else '0'
+            main_func = main_func.replace(variable, mean)
+            main_func = main_func.replace(f'!0', '')
+            main_func = main_func.replace(f'!1', '0')
+            main_func = main_func.replace(f'1', '')
+        test = main_func.split('+')[1:]
+        test = list(filter(lambda x: '0' not in x,  test))
+        main_func = ''.join(test)
+        count_positive = main_func.count(f' {variable_that_not_in_impicant} ')
+        count_negative = main_func.count(f' !{variable_that_not_in_impicant} ')
+        if count_negative == count_positive:
+            ans.append(implicant.copy())
+
+    return ans
 
 
 if __name__ == "__main__":
-    infunc = input('Enter your function: ')
+    # infunc = input('Enter your function: ')
+    infunc = '((a+b)*(c+d))'
     infunc = infunc.replace("=", "==")
     variables = sorted(set(re.findall(r"[A-Za-z]", infunc)))
     vars_for_table = vars_for_truth_table(variables)
     truth_table_answer = calculate_truth_table(infunc[:], vars_for_table, variables)
-    print_table(vars_for_table, truth_table_answer, variables)
-    print('Disjunctive_normal_form')
-    print_disjunctive_normal_form(vars_for_table, truth_table_answer)
-    print('Conjunctive_normal_form')
-    print_conjunctive_normal_form(vars_for_table, truth_table_answer)
-    print('Disjunctive_number_form')
-    print_disjunctive_number_form(truth_table_answer)
-    print('Conjunctive_number_form')
-    print_conjunctive_number_form(truth_table_answer)
-    print('Index form ')
-    ans = print_index_form(truth_table_answer)
-    print(f'F {ans} сднф')
+
+    disjunctive_normal_form = get_disjunctive_normal_form(vars_for_table, truth_table_answer)
+    conjunctive_normal_form = get_conjunctive_normal_form(vars_for_table, truth_table_answer)
+
+    a = gluing(disjunctive_normal_form)
+    b = drop_extra_impicants(a, variables)
+    print(b)
